fe107a80e5aed6f06e5e13dabbdee1bddbc45492
forwardforever <jnbfeng@163.com>
2017-09-27 16:05:48 +0800

    commit code

--- GitHubStudy/app/src/main/java/com/bewei/github/MainActivity.java
+++ GitHubStudy/app/src/main/java/com/bewei/github/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		           //这是clone下来的代码 修改之后继续提交
+        
+
     }
 }
